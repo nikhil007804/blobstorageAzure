07b25866fbfda410aa1e45921186f1f893ffd1dc
nikhil007804 <addepallenikhilvarma@gmail.com>
2025-07-02 17:13:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "demo-rg"
-    storage_account_name  = "prathyushatfstate1234"
-    container_name        = "tfstate"
-    key                   = "blob-container.terraform.tfstate"
-  }
-}
+
